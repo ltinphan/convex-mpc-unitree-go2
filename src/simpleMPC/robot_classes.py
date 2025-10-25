79c064dfc3649d7b83502d1aa36aaaf28d4c629b
Elijah Chan <chanwaichong0352@gmail.com>
2025-10-25 00:23:31 -0400

    edited

--- src/simpleMPC/robot_classes.py
+++ src/simpleMPC/robot_classes.py
@@ -9,11 +9,14 @@ class RigidBodyState:
     x_vel: float = 0.0
     y_vel: float = 0.0
     z_vel: float = 0.0
+    roll: float = 0.0
+    pitch: float = 0.0
+    yaw: float = 0.0
     roll_rate: float = 0.0
     pitch_rate: float = 0.0
     yaw_rate: float = 0.0
 
-
+@dataclass
 class RigidBodyTraj:
     time: np.ndarray = np.empty(0)
     x_pos_ref: np.ndarray = np.empty(0)
@@ -22,6 +25,17 @@ class RigidBodyTraj:
     x_vel_ref: np.ndarray = np.empty(0)
     y_vel_ref: np.ndarray = np.empty(0)
     z_vel_ref: np.ndarray = np.empty(0)
+    roll_ref: np.ndarray = np.empty(0)
+    pitch_ref: np.ndarray = np.empty(0)
+    yaw_ref: np.ndarray = np.empty(0)
     roll_rate_ref: np.ndarray = np.empty(0)
     pitch_rate_ref: np.ndarray = np.empty(0)
     yaw_rate_ref: np.ndarray = np.empty(0)
+    fl_foot_placement: np.ndarray = np.empty(0)
+    fr_foot_placement: np.ndarray = np.empty(0)
+    rl_foot_placement: np.ndarray = np.empty(0)
+    rr_foot_placement: np.ndarray = np.empty(0)
+    fl_foot_placement_vec: np.ndarray = np.empty(0)
+    fr_foot_placement_vec: np.ndarray = np.empty(0)
+    rl_foot_placement_vec: np.ndarray = np.empty(0)
+    rr_foot_placement_vec: np.ndarray = np.empty(0)
