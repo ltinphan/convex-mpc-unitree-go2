5abb2d858c77854e72576a713061d8e2386069ae
Elijah Chan <chanwaichong0352@gmail.com>
2025-11-06 17:18:29 -0800

    version 0.1.0

---
+++ simpleMPC/__init__.py
@@ -0,0 +1,25 @@
+"""
+simpleMPC - A simple Model Predictive Control library for the Unitree Go2.
+
+This package provides functionalities for controlling the Unitree Go2 robot using Model Predictive Control (MPC).
+"""
+
+# You can define the version of your package here if you'd like
+__version__ = "0.1.0"
+
+# Import relevant components/modules of your package
+# For example, if you have other Python files like `controller.py` and `planner.py`, you can import them here:
+# from .controller import Controller
+# from .planner import Planner
+
+# Optionally, you can provide a basic API for your package here
+# Example: Initialize a high-level object that will provide an easy interface for users
+# class SimpleMPC:
+#     def __init__(self):
+#         self.controller = Controller()
+#         self.planner = Planner()
+
+# Define other package-level variables or utility functions if necessary
+# For example, a utility function:
+# def get_default_controller_params():
+#     return {'max_iter': 100, 'tolerance': 1e-3}
